c3b568319be37211d46b40b1e30d3a646fc8a597
floflo75 <rosaflo89@hotmail.com>
2016-05-18 10:40:16 +0200

    First commit

---
+++ html_css/javascript/ExCheckbox.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+function valid()
+{ document.forms.frm.c1.checked = 1; 
+document.forms.frm.c2.checked = 2;
+document.forms.frm.c3.checked = 3;
+document.forms.frm.c4.checked = 4;
+}
+
+function valid()
+{ alert("Coucou");
+}
